f0e15432c1299658c9a8de1dd0b7a1b694b060f0
turanszkik <turanszkik@gmail.com>
2024-05-20 19:25:27 +0200

    Gallery component works!

--- src/app/pages/galery-component/galery-component.component.ts
+++ src/app/pages/galery-component/galery-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import ImageViewer from 'awesome-image-viewer';
 
 @Component({
   selector: 'app-galery-component',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './galery-component.component.css'
 })
 export class GaleryComponent {
+
+  imageIndexes: any[];
+
+  data = [{
+    mainUrl: 'cdn.com/img1.jpg',
+    thumbnailUrl: 'cdn.com/img1.min.jpg',
+    description: 'Steel Wool',
+  },
+  {
+    mainUrl: 'cdn.com/img2.jpg',
+    thumbnailUrl: 'cdn.com/img2.min.jpg',
+    description: 'Beach',
+  }];
+
+  constructor() {
+    this.imageIndexes = Array(32).fill(35).map((x, i) => ({
+      mainUrl: `../../../assets/images/galery/full/${i + 1}.jpg`,
+      thumbnailUrl: `../../../assets/images/galery/${i + 1}.jpg`,
+      description: ''
+    })
+    );
+  }
+
+  openImage(image: number): void {
+    new ImageViewer({
+      images: this.imageIndexes,
+      currentSelected: image
+    });
+  }
 
 }
