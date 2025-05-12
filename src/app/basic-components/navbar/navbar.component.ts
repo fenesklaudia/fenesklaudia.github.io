58fcecd31b143b320391213784c97c8d267adcdf
turanszkik <turanszkik@gmail.com>
2025-05-12 20:39:33 +0200

    UI changes
    - Icon + navbar color
    - Main tab name
    - Navbar scroll
    - Galery rename

--- src/app/basic-components/navbar/navbar.component.ts
+++ src/app/basic-components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+declare const window: any;
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  @ViewChild('navbar') navbar: ElementRef;
 
+  ngAfterViewInit() {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        this.navbar.style.top = "0";
+      } else {
+        if (currentScrollPos > 50) {
+          this.navbar.style.top = "-100rem";
+        }
+      }
+      prevScrollpos = currentScrollPos;
+    }
+  }
 }
