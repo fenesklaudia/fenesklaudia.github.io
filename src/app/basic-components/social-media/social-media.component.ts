991de798a558dd1b605730f0cb2259e9c7b18a42
turanszkik <turanszkik@gmail.com>
2025-06-29 19:34:18 +0200

    Socal media fix

--- src/app/basic-components/social-media/social-media.component.ts
+++ src/app/basic-components/social-media/social-media.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+declare var FB: any; // Declare FB global
 
 @Component({
     selector: 'app-social-media',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
     styleUrl: './social-media.component.css',
     standalone: false
 })
-export class SocialMediaComponent {
+export class SocialMediaComponent implements AfterViewInit {
+
+    ngAfterViewInit(): void {
+        FB.XFBML.parse();
+
+        const existing = document.querySelector('script[src="https://www.tiktok.com/embed.js"]');
+        if (existing) {
+            existing.remove();
+        }
+
+        const script = document.createElement('script');
+        script.src = 'https://www.tiktok.com/embed.js';
+        script.async = true;
+        document.body.appendChild(script);
+    }
 
 }
