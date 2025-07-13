57f68b3d18f85148e983e144077c05045ec872fd
turanszkik <turanszkik@gmail.com>
2025-07-13 14:48:46 +0200

    About me changes
    https://github.com/fenesklaudia/fenesklaudia.github.io/issues/6
    
    Extra:
    galery images 2 per line on mobile

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import ImageViewer from "awesome-image-viewer";
 
 @Component({
-  selector: 'app-about-me',
-  standalone: false,
-  templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.css'
+    selector: 'app-about-me',
+    standalone: false,
+    templateUrl: './about-me.component.html',
+    styleUrl: './about-me.component.css'
 })
 export class AboutMeComponent {
 
+    public imageIndexes: any[];
+
+    constructor() {
+        this.imageIndexes = [{
+            mainUrl: `../../../assets/images/about-me/about-me-1.jpg`,
+            thumbnailUrl: `../../../assets/images/about-me/about-me-1.jpg`,
+            description: ''
+        }, {
+            mainUrl: `../../../assets/images/about-me/about-me-2.jpg`,
+            thumbnailUrl: `../../../assets/images/about-me/about-me-2.jpg`,
+            description: ''
+        }, {
+            mainUrl: `../../../assets/images/about-me/about-me-3.jpg`,
+            thumbnailUrl: `../../../assets/images/about-me/about-me-3.jpg`,
+            description: ''
+        }, {
+            mainUrl: `../../../assets/images/about-me/about-me-4.jpeg`,
+            thumbnailUrl: `../../../assets/images/about-me/about-me-4.jpeg`,
+            description: ''
+        }]
+    }
+
+    openImage(image: number): void {
+        new ImageViewer({
+            images: this.imageIndexes,
+            currentSelected: image
+        });
+    }
 }
